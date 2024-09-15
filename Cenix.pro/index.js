5ec6b0222d4ba265175eb538ed72ef8c223233a5
denis__laptop <amirowdenis@yandex.ru>
2024-09-15 10:48:04 +0300

    small_refactoring

--- Cenix.pro/index.js
+++ Cenix.pro/index.js
@@ -1,93 +1,91 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+
 const viewportSettings = {
     width: 1920,
     height: 1080,
     deviceScaleFactor: 1,
 }
 const timeoutCheckBrowser = 15000;
+const selectors = {
+    regionList: '.UiRegionListBase_list__cH0fK',
+    regionMenu: '.UiHeaderHorizontalBase_region__2ODCG', 
+    productStars: '.ActionsRow_stars__EKt42',
+    productReviews: '.ActionsRow_reviews__AfSj_',
+    productPriceInfo: '.PriceInfo_root__GX9Xp',
+    regionSelect: '.UiRegionListBase_list__cH0fK',
+};
+const screenshotName = 'screenshot.jpg';
+const fileResultName = 'product.txt';
 
 
 (async () => {
-
     const link = process.argv[2]
     const region = process.argv[3]
-
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage({ waitUntil: 'networkidle0' });
 
     await page.goto(link);
     await page.setViewport(viewportSettings);
+    
     await new Promise(r => setTimeout(r, timeoutCheckBrowser));
-    await page.waitForSelector('.UiHeaderHorizontalBase_region__2ODCG', { timeout: 10000 })
+
+    await page.waitForSelector(selectors.regionMenu)
         .then(element => { element.click() })
         .catch(error => console.log(error));
-    await page.waitForSelector(".UiRegionListBase_list__cH0fK", { timeout: 10000 })
-        .then(async (element) => {
+    
+    await page.waitForSelector(selectors.regionList)
+        .then(async () => {
             let RegionIsFinded = false;
-            const regionList = await page.$eval('.UiRegionListBase_list__cH0fK', erw => erw.innerHTML)
+            const regionList = await page.$eval(selectors.regionList, erw => erw.innerHTML)
             const regionListSplit = regionList.split('</li>');
 
             for (let i = 0; i < regionListSplit.length; i++) {
                 const regionFromPage = regionListSplit[i].slice(regionListSplit[i].indexOf('>') + 1,)
                 if (region == regionFromPage) {
                     RegionIsFinded = true
-                    await new Promise(r => setTimeout(r, 2000));
-                    await page.waitForSelector('.UiRegionListBase_list__cH0fK li:nth-child(2)')
-                    await page.click(`.UiRegionListBase_list__cH0fK li:nth-child(${i + 1})`);
+                    // await new Promise(r => setTimeout(r, 2000));
+                    await page.waitForSelector(`${selectors.regionSelect} li:nth-child(2)`);
+                    await page.click(`${selectors.regionSelect} li:nth-child(${i + 1})`);
                 }
             }
 
             if (!RegionIsFinded) {
-                console.error(`Регион ${region} не найден!`)
+                console.error(`Region ${region} is not founded`)
                 await browser.close();
             }
         })
 
+    const priceInfoTextContent = await getTextContentForPuppeteer(page, selectors.productPriceInfo, { timeout: 2000 });
+    const reviews = await getTextContentForPuppeteer(page, selectors.productReviews);
+    const price = parsePrice(priceInfoTextContent).price;
+    const oldPrice = parsePrice(priceInfoTextContent).oldPrice;
 
-    async function getPrice(page, selector) {
-        try {
-            await page.waitForSelector(selector);
-            const res = await page.$eval(selector, erw => erw.textContent);
-            return res;
-        } catch (error) {
-            return null;
-        }
-    }
-
-
-    const res = await getPrice(page, '.PriceInfo_root__GX9Xp', { timeout: 2000 });
-    const z = await getPrice(page, '.ActionsRow_reviews__AfSj_')
-    const a = parsePrice(res).price
-    const x = parsePrice(res).oldPrice
-
-    await page.waitForSelector('.ActionsRow_stars__EKt42');
-    const y = await page.$eval('.ActionsRow_stars__EKt42', erw => erw.title)
+    await page.waitForSelector(selectors.productStars);
+    const stars = await page.$eval(selectors.productStars, erw => erw.title)
 
     await new Promise(r => setTimeout(r, 1000))
         .then(async () => {
             await page.screenshot({
-                path: 'screenshot.jpg'
+                path: screenshotName
             });
-            console.info('Парсинг завершился успешно')
+            console.info('The parsing was completed successfully!')
             await browser.close();
         });
 
-    fs.writeFile('product.txt', `price=${cleanString(a)}\n`
-        + `priceOld=${cleanString(x)}\nrating=${cleanString(y)}\nreviewCount=${cleanString(z)}\n`, (err) => {
-            err ? console.log(err) : console.info('Успешная запись в файл')
+    fs.writeFile(fileResultName, `price=${cleanString(price)}\n`
+        + `priceOld=${cleanString(oldPrice)}\nrating=${cleanString(stars)}\nreviewCount=${cleanString(reviews)}\n`, (err) => {
+            err ? console.log(err) : console.info('Successful writing to a file!')
         });
 })();
-
 
 
 function cleanString(inputString) {
     console.log(inputString)
     if (!inputString) return null;
     let cleanedString = inputString.replace(/[^0-9,.]/g, '');
-    cleanedString = cleanedString.replace(/,/g, '.');
-    return cleanedString;
+    return cleanedString.replace(/,/g, '.');
 }
 
 
@@ -109,5 +107,16 @@ function parsePrice(str) {
         } else {
             throw new Error('Invalid input string format');
         }
+    }
+}
+
+
+async function getTextContentForPuppeteer(page, selector) {
+    try {
+        await page.waitForSelector(selector);
+        const res = await page.$eval(selector, erw => erw.textContent);
+        return res;
+    } catch (error) {
+        return null;
     }
 }
